603105d8327853a89c7d4612f7de3c151021d5cc
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-26 19:29:57 +0100

    The implementation of this specification should be fairly easy. All we need to do is set the constructor argument to the location variable

--- src/main/java/gois/study/tdjdship/Ship.java
+++ src/main/java/gois/study/tdjdship/Ship.java
@@ -1,5 +1,13 @@
 package gois.study.tdjdship;
 
 public class Ship {
+    private final Location location;
 
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
