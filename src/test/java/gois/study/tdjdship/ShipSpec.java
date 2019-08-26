279b1504ddc1a881dedb836610211331f8ef92a1
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-26 19:26:03 +0100

    This was an easy one. We're just checking whether the Location object we're passing as the Ship constructor is stored and can be accessed through the location getter

--- src/test/java/gois/study/tdjdship/ShipSpec.java
+++ src/test/java/gois/study/tdjdship/ShipSpec.java
@@ -1,5 +1,14 @@
 package gois.study.tdjdship;
 
-public class ShipSpec {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class ShipSpec {
+    @Test
+    public void whenInstantiatedThenLocationIsSet() {
+        Location location = new Location(new Point(21, 13), Direction.NORTH);
+        Ship ship = new Ship(location);
+        assertEquals(ship.getLocation(), location);
+    }
 }
