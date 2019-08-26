a7d175a9a84e38ca4fd8a007a338d2ab34ce4897
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-26 19:34:31 +0100

    Refactoring We know that we'll need to instantiate Ship for every spec, so we might as well refactor the specification class by adding the @Before annotation.
    No new behavior has been introduced. We just moved part of the code to the @Before annotation in order to avoid duplication, which would be produced by the rest of the specifications that we are about to write. Now, every time a test is run, the ship object will be instantiated with location as the argument

--- src/test/java/gois/study/tdjdship/ShipTest.java
+++ src/test/java/gois/study/tdjdship/ShipTest.java
@@ -1,14 +1,23 @@
 package gois.study.tdjdship;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ShipTest {
+
+    private Ship ship;
+    private Location location;
+
+    @Before
+    public void beforeTest() {
+        location = new Location(new Point(21, 13), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
     @Test
     public void whenInstantiatedThenLocationIsSet() {
-        Location location = new Location(new Point(21, 13), Direction.NORTH);
-        Ship ship = new Ship(location);
         assertEquals(ship.getLocation(), location);
     }
 }
