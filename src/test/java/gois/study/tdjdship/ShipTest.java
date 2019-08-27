1b72f48bf31109a9c8c067c40f6f1d99eeffdd08
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-27 17:29:44 +0100

    Testing

--- src/test/java/gois/study/tdjdship/ShipTest.java
+++ src/test/java/gois/study/tdjdship/ShipTest.java
@@ -144,4 +144,13 @@ public class ShipTest {
         ship.receiveCommands("rflb");
         assertEquals(ship.getLocation(), expected);
     }
+
+    @Test
+    public void whenReceiveCommandsThenOForOkAndXForObstacle() {
+        List<Point> obstacles = new ArrayList<>();
+        obstacles.add(new Point(location.getX() + 1, location.getY()));
+        ship.getPlanet().setObstacles(obstacles);
+        String status = ship.receiveCommands("rflb");
+        assertEquals(status, "OXOO");
+    }
 }
