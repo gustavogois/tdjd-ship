eeafae19a644b8b7df639d0478989deaf9e2874f
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-27 10:06:17 +0100

    Implementation If you tried to write specifications and the implementation by yourself and if you followed the simplicity rule, you probably had to refactor your code a couple of times in order to get to the final solution. Simplicity is the key and refactoring is often a welcome necessity. When refactoring, remember that all specifications must be passing all the time.
    Refactor only after all the tests have passed.
    
    Benefits: refactoring is safe.
    
    If all the implementation code that can be affected has tests and if they are all passing, it is relatively safe to refactor. In most cases, there is no need for new tests; small modifications to existing tests should be enough. The expected outcome of refactoring is to have all the tests passing both before and after the code is modified.

--- src/main/java/gois/study/tdjdship/Ship.java
+++ src/main/java/gois/study/tdjdship/Ship.java
@@ -27,37 +27,22 @@ public class Ship {
         location.turnRight();
     }
 
-    /*
-    We already spoke about the importance of writing the simplest possible code that passes the specification.
-
-    Write the simplest code to pass the test. This ensures a cleaner and clearer design and avoids unnecessary features
-
-    The idea is that the simpler the implementation, the better and easier it is to maintain the product. The idea adheres to the KISS principle. It states that most systems work best if they are kept simple rather than made complex; therefore, simplicity should be a key goal in design and unnecessary complexity should be avoided.
-    This is a good opportunity to apply this rule. You might be inclined to write a piece of code similar to the following:
-
     public void receiveCommands(String commands) {
-      if (commands.charAt(0) == 'f') {
-        moveForward();
-      }
-    }
-    In this example code, we are verifying whether the first character is f and, if it is, invoking the moveForward method. There are many other variations that we can do. However, if we stick to the simplicity principle, a better solution would be the following
-    This is the simplest and shortest possible code that will make the specification pass. Later on, we might end up with something closer to the first version of the code; we might use some kind of a loop or come up with some other solution when things become more complicated. As for now, we are concentrating on one specification at a time and trying to make things simple. We are attempting to clear our mind by focusing only on the task at hand.
-    */
-
-    public void receiveCommands(String command) {
-        switch(command) {
-            case "f":
-                moveForward();
-                break;
-            case "b":
-                moveBackward();
-                break;
-            case "l":
-                turnLeft();
-                break;
-            case "r":
-                turnRight();
-                break;
+        for (char command : commands.toCharArray()) {
+            switch(command) {
+                case 'f':
+                    moveForward();
+                    break;
+                case 'b':
+                    moveBackward();
+                    break;
+                case 'l':
+                    turnLeft();
+                    break;
+                case 'r':
+                    turnRight();
+                    break;
+            }
         }
     }
 }
